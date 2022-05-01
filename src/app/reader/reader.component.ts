bc93eed83e9b5b9c9216356667e86a6ef6b8a45f
palikollar <palkollar1995@gmail.com>
2022-05-01 08:17:15 +0200

    reset project

---
+++ src/app/reader/reader.component.ts
@@ -0,0 +1,7 @@
+import { Component } from "@angular/core";
+
+@Component({selector: 'ns-reader',
+    templateUrl: './reader.component.html',
+    styleUrls: ['./reader.component.scss']
+})
+export class ReaderComponent {}
