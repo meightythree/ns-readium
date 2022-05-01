dd960a1011faed7a21a97ea2f2edb65ce16972ad
palikollar <palkollar1995@gmail.com>
2022-05-01 08:23:24 +0200

    Added nota webview

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { NativeScriptModule } from '@nativescript/angular'
 
 import { AppComponent } from './app.component'
 import { ReaderComponent } from "./reader/reader.component";
+import { WebViewExtModule } from "@nota/nativescript-webview-ext/angular"
 
 @NgModule({
   bootstrap: [AppComponent],
-  imports: [NativeScriptModule],
+  imports: [NativeScriptModule, WebViewExtModule],
   declarations: [AppComponent, ReaderComponent],
   providers: [],
   schemas: [NO_ERRORS_SCHEMA],
